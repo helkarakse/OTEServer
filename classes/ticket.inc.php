6d1d83738d3219dc64de80f654d92ebb0ecb090c
helkarakse <nimcuron@gmail.com>
2013-12-22 07:40:07 +0800

    Fixed issue with lastErrorMsg() call on ticket class

--- classes/ticket.inc.php
+++ classes/ticket.inc.php
@@ -57,7 +57,7 @@ class Ticket {
 		$create_date = date ( 'Y-m-d H:i:s' );
 		return $this->db->exec ( 
 				"INSERT INTO Tickets VALUES(NULL, '$creator', '$description', '$position', 'new', 'mod', '', '$create_date', '$create_date')" ) or
-				 die ( $db->lastErrorMsg () );
+				 die ( $this->db->lastErrorMsg () );
 	}
 
 	function getTickets($status) {
