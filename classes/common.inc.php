12d32ee1f1d9c65c05036b8a7bf2c21ab760b9f3
helkarakse <nimcuron@gmail.com>
2013-12-21 12:56:37 +0800

    Added some extra helper functions, showSuccess, showError, outputJson

--- classes/common.inc.php
+++ classes/common.inc.php
@@ -9,3 +9,21 @@ function getVar($variable) {
 function getPostVar($variable) {
 	return isset ( $_POST [$variable] ) ? $_POST [$variable] : "";
 }
+
+// output the array as a json formatted string
+function outputJson($data) {
+	echo (json_encode ( array ("result" => $data,"success" => true 
+	) ));
+}
+
+// output the error as a json formatted string
+function showError() {
+	echo (array ("result" => array (),"success" => false 
+	));
+}
+
+// output the success as a json formatted string
+function showSuccess() {
+	echo (array ("result" => array (),"success" => true 
+	));
+}
