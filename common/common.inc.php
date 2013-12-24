08d5d40b8f135ef0359699d4db1bf4dc8898aac5
helkarakse <nimcuron@gmail.com>
2013-12-24 16:21:35 +0800

    Added fileWrite, fileRead, outputEcho functions to common

--- common/common.inc.php
+++ common/common.inc.php
@@ -12,18 +12,38 @@ function getPostVar($variable) {
 
 // output the array as a json formatted string
 function outputJson($data) {
-	echo (json_encode ( array ("result" => $data,"success" => true 
+	echo (json_encode ( array (
+		"result" => $data,"success" => true 
 	), JSON_NUMERIC_CHECK ));
+}
+
+function outputEcho($text) {
+	echo ($text);
 }
 
 // output the error as a json formatted string
 function showError() {
-	echo (json_encode ( array ("result" => array (),"success" => false 
+	echo (json_encode ( array (
+		"result" => array (),"success" => false 
 	), JSON_NUMERIC_CHECK ));
 }
 
 // output the success as a json formatted string
 function showSuccess() {
-	echo (json_encode ( array ("result" => array (),"success" => true 
+	echo (json_encode ( array (
+		"result" => array (),"success" => true 
 	), JSON_NUMERIC_CHECK ));
+}
+
+function fileWrite($filename, $text) {
+	$handle = fopen ( $filename, "w" ) or die ( "Error: Could not open the file for writing." );
+	fwrite ( $handle, $text );
+	fclose ( $handle );
+}
+
+function fileRead($filename) {
+	$handle = fopen ( $filename, "r" );
+	$data = fread ( $handle, filesize ( $filename ) );
+	fclose ( $handle );
+	return $data;
 }
