d9f7d7a077e10507eaaaba027ae04730d3540bfb
helkarakse <nimcuron@gmail.com>
2013-12-26 23:42:20 +0800

    Completed Upload controller

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -3,12 +3,29 @@ if (! defined ( 'BASEPATH' )) exit ( 'No direct script access allowed' );
 class Upload extends CI_Controller {
 	
 	// upload/put/server/type
-	public function put($server, $type) {
+	public function put() {
+		$server = $this->input->get ( "server" );
+		$type = $this->input->get ( "type" );
 		$timeNow = date ( "r", time () );
 		
-		$json = urldecode ( $this->input->post ( "json" ) );
-		$array = json_decode ( $text );
+		// decode and set the updated key
+		$array = json_decode ( urldecode ( $this->input->post ( "json" ) ) );
+		$playerArray = json_decode ( urldecode ( $this->input->post ( "players" ) ) );
 		$array [5] ["updated"] = $timeNow;
+		
+		// save the tps data and player list to db
+		foreach ( $array [0] as $key => $value ) {
+			$tps = $value;
+		}
+		
+		$rowId = $this->tick->insert_tps ( $tps, $timeNow, $server, $type );
+		$this->tick->insert_players ( $rowId, $playerArray );
+		
+		// re encode the json
+		$text = stripslashes ( json_encode ( $array ) );
+		
+		// save the json to file
+		$this->tick->write_tick_data ( $text, $server, $type );
 		
 		echo ("Updated at: " . $timeNow);
 	}
