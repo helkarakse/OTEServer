962433241058592279649b904d395bf9c7e1f062
helkarakse <nimcuron@gmail.com>
2013-12-27 19:04:49 +0800

    Completed the add_ticket method for the ticket controller

--- application/controllers/ticket.php
+++ application/controllers/ticket.php
@@ -1,5 +1,28 @@
 <?php
 
-if (! defined ( 'BASEPATH' )) exit ( 'No direct script access allowed' );
-class Ticket extends CI_Controller {
-}
+	if (! defined('BASEPATH'))
+		exit ('No direct script access allowed');
+
+	class Ticket extends CI_Controller {
+		function get_tickets() {
+
+		}
+
+		// adds a new ticket to the db
+		function add_ticket() {
+			$creator = $this->input->post("creator");
+			$description = $this->input->post("description");
+			$position = $this->input->post("position");
+
+			if (! empty($creator) && ! empty($description) && ! empty($position)) {
+				$success = $this->ticket->create_ticket($creator, $description, $position);
+				$this->outputJson(array(), $success);
+			} else {
+				$this->outputJson(array(), FALSE);
+			}
+		}
+
+		private function outputJson($result, $success) {
+			echo(json_encode(array("result" => $result, "success" => $success)));
+		}
+	}
