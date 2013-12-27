bec75ae8e7a4c13ce0f83f9dab9faa780562f73d
helkarakse <nimcuron@gmail.com>
2013-12-27 18:40:10 +0800

    Added the methods for the auth model

--- application/models/auth.php
+++ application/models/auth.php
@@ -5,4 +5,24 @@
 		function __construct() {
 			parent::__construct();
 		}
+
+		// returns the authentication data for username
+		function get_auth($username) {
+			$query = $this->db->get_where("TicketAuth", array("name" => $username));
+			if ($query->num_rows() > 0) {
+				return $query->first_row();
+			} else {
+				return array();
+			}
+		}
+
+		// returns the entire authentication package
+		function get_all_auth() {
+			$query = $this->db->get("TicketAuth");
+			if ($query->num_rows() > 0) {
+				return $query->result();
+			} else {
+				return array();
+			}
+		}
 	}
