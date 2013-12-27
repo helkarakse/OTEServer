940e33766db8a37d2c6cf97b1195caa4152c2227
helkarakse <nimcuron@gmail.com>
2013-12-27 18:05:54 +0800

    Added auth, issue model files and added them to the autoloader

---
+++ application/models/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+	class Auth extends CI_Model {
+
+		function __construct() {
+			parent::__construct();
+		}
+	}
---
+++ application/models/issue.php
@@ -0,0 +1,8 @@
+<?php
+
+	class Issue extends CI_Model {
+
+		function __construct() {
+			parent::__construct();
+		}
+	}
