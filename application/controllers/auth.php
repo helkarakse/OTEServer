e491176e3d526df7d490e557f57d1d70b6d8e1f7
helkarakse <nimcuron@gmail.com>
2013-12-29 00:14:45 +0800

    Added get_auth and get_auth_package methods to auth controller

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -3,46 +3,22 @@
 	if (! defined('BASEPATH'))
 		exit ('No direct script access allowed');
 
-	class Ticket extends CI_Controller {
-		function get_user_tickets() {
-			$this->load->model("ticket_model");
+	class Auth extends CI_Controller {
+		function get_auth() {
+			$this->load->model("auth_model");
 			$username = $this->input->get_post("name");
 			if (! empty($username)) {
-				$output = array();
-				$data = $this->ticket_model->get_user_tickets($username);
-				foreach ($data as $row) {
-					$output[] = array(
-						"description" => $row->description, "status" => $row->status,
-						"time_ago"    => pretty_time(strtotime($row->create_date))
-					);
-				}
-
-				output_json($output, TRUE);
+				output_json($this->auth_model->get_auth($username), TRUE);
 			} else {
 				output_json(array(), FALSE);
 			}
 		}
 
-		function get_user_ticket_count() {
-			$this->load->model("ticket_model");
+		function get_auth_package() {
+			$this->load->model("auth_model");
 			$username = $this->input->get_post("name");
 			if (! empty($username)) {
-				output_json($this->ticket_model->get_user_ticket_count($username), TRUE);
-			} else {
-				output_json(array(), FALSE);
-			}
-		}
-
-		// adds a new ticket to the db
-		function add_ticket() {
-			$this->load->model("ticket_model");
-			$creator = $this->input->get_post("creator");
-			$description = $this->input->get_post("description");
-			$position = $this->input->get_post("position");
-
-			if (! empty($creator) && ! empty($description) && ! empty($position)) {
-				$this->ticket_model->create_ticket($creator, $description, $position);
-				output_json(array(), TRUE);
+				output_json($this->auth_model->get_all_auth(), TRUE);
 			} else {
 				output_json(array(), FALSE);
 			}
