257d79da704cddd0fffdc6678762c432aaad6562
helkarakse <nimcuron@gmail.com>
2013-12-29 14:42:45 +0800

    Updated the encryption key for session

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '5Q5QV5Ym6wwN234N9oruAc3V71GNM327';
 
 /*
 |--------------------------------------------------------------------------
