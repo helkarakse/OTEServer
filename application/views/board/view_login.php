524b1bf702d1383717b7c1c1d154513f1c0b3fba
helkarakse <nimcuron@gmail.com>
2013-12-29 14:35:15 +0800

    Added login form to view_login

--- application/views/board/view_login.php
+++ application/views/board/view_login.php
@@ -3,26 +3,32 @@
 <head>
 	<title>TickProfiler Admin Portal></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/board.css">
 	<link rel="stylesheet" type="text/css" href="css/horizontal-menu.css">
 	<link rel="stylesheet" type="text/css" href="css/vertical-menu.css">
 	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 </head>
 <body>
 <div class="formCenter">
-	<form method="POST" action="index.php">
-		Username:
-		<br/>
-		<input type="text" name="username"/>
-		<br/>
-		<br/>
-		Password:
-		<br/>
-		<input type="password" name="password"/>
-		<br/>
-		<br/>
-		<input type="submit" id="submit" value="Submit"/>
-	</form>
+	<?php echo form_open('c=board&m=index'); ?>
+	<?php echo form_fieldset('Login'); ?>
+
+	<div class="textfield">
+		<?php echo form_label('Username', 'user_name'); ?>
+		<?php echo form_input('user_name'); ?>
+	</div>
+
+	<div class="textfield">
+		<?php echo form_label('Password', 'user_pass'); ?>
+		<?php echo form_password('user_pass'); ?>
+	</div>
+
+	<div class="buttons">
+		<?php echo form_submit('login', 'Login'); ?>
+	</div>
+
+	<?php echo form_fieldset_close(); ?>
+	<?php echo form_close(); ?>
 </div>
 </body>
 </html>
