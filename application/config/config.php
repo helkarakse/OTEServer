96379943b1fbe3eb3a056cc3e1f120c54b5a0b5c
helkarakse <nimcuron@gmail.com>
2013-12-29 15:24:40 +0800

    Corrected wrong baseurl for config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://dev.otegamers.com/dev/index.php';
+$config['base_url']	= 'http://dev.otegamers.com/dev/';
 
 /*
 |--------------------------------------------------------------------------
