12f97b5f4c15fb2b51ceaa72369e2fee2dc5f225
helkarakse <nimcuron@gmail.com>
2013-12-30 16:39:12 +0800

    Updated the js and the css for the table display

--- js/tick_board.js
+++ js/tick_board.js
@@ -1,13 +1,15 @@
-$(document).ready(function() {
-	$("div.tableCSS").hide();
-	$("div.tableCSS:first").show();
-	$("li.menuLink").click(function() {
-		var toShow = $(this).attr("display");
-		$("div.tableCSS").fadeOut().delay(250);
-		$("div.tableCSS").each(function(index) {
-			if (this.id == toShow) {
-				$(this).fadeIn();
-			}
-		});
-	});
+$(document).ready(function () {
+    setInterval("location.reload(true)", 1000 * 60 * 2);
+
+    $("div.tableCSS").hide();
+    $("div.tableCSS:first").show();
+    $("li.menuLink").click(function () {
+        var toShow = $(this).attr("display");
+        $("div.tableCSS").fadeOut().delay(250);
+        $("div.tableCSS").each(function () {
+            if (this.id == toShow) {
+                $(this).fadeIn();
+            }
+        });
+    });
 });
