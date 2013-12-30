1bc941cc0bf108e5ad7336d912bc0833ff33bcd5
helkarakse <nimcuron@gmail.com>
2013-12-30 16:37:51 +0800

    Updated the login view file for the new template

--- application/views/admin/view_login.php
+++ application/views/admin/view_login.php
@@ -1,22 +1,20 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-	<title>TickProfiler Admin Portal></title>
+	<title>Admin Portal</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-	<link rel="stylesheet" type="text/css" href="css/board.css">
-	<link rel="stylesheet" type="text/css" href="css/horizontal-menu.css">
-	<link rel="stylesheet" type="text/css" href="css/vertical-menu.css">
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<link rel="stylesheet" type="text/css" href="./css/horizontal-menu.css">
+	<link rel="stylesheet" type="text/css" href="./css/vertical-menu.css">
+	<script type="text/javascript"
+	        src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 </head>
 <body>
-<h1>Tutorial: Simple Login Form</h1>
-
-
 <div class="formCenter">
 	<?php if ($this->session->flashdata('message')) : ?>
 		<p><?php echo $this->session->flashdata('message'); ?></p>
 	<?php endif; ?>
-	
+
 	<?php echo form_open('c=admin&m=index'); ?>
 	<?php echo form_fieldset('Login'); ?>
 
