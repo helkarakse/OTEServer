ee305a683533ffb6e84043fed4b14dd3a96173c2
helkarakse <nimcuron@gmail.com>
2013-12-30 16:38:56 +0800

    Fixed site redirection by using site_url with redirect method

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -14,70 +14,33 @@
 			$this->form_validation->set_rules('user_pass', 'password', 'required');
 			$this->form_validation->set_error_delimiters('<em>', '</em>');
 
-			if ($this->input->post('login')) {
-				if ($this->form_validation->run()) {
-					$user_name = $this->input->post('user_name');
-					$user_pass = $this->input->post('user_pass');
+			if ($this->input->post('login') && $this->form_validation->run()) {
+				$user_name = $this->input->post('user_name');
+				$user_pass = $this->input->post('user_pass');
 
-					if (array_key_exists($user_name, $credentials)) {
-						if ($user_pass == $this->encrypt->decode($credentials[$user_name]['password'])) {
-							$this->session->set_userdata(array("is_logged_in" => TRUE));
-							redirect("c=admin&m=main", "refresh");
-						} else {
-							$this->session->set_flashdata('message', 'Incorrect password.');
-							redirect("c=admin&m=index", "refresh");
-						}
+				if (array_key_exists($user_name, $credentials)) {
+					if ($user_pass == $this->encrypt->decode($credentials[$user_name]['password'])) {
+						$this->session->set_userdata(array("is_logged_in" => TRUE));
+						redirect(site_url(array("c" => "admin", "m" => "main")), "refresh");
 					} else {
-						$this->session->set_flashdata('message', 'A user does not exist for the username specified.');
-						redirect("c=admin&m=index", "refresh");
+						$this->session->set_flashdata('message', 'Incorrect password.');
+						redirect(site_url(array("c" => "admin", "m" => "index")), "refresh");
 					}
+				} else {
+					$this->session->set_flashdata('message', 'A user does not exist for the username specified.');
+					redirect(site_url(array("c" => "admin", "m" => "index")), "refresh");
 				}
+			} else {
+				$this->load->view("admin/view_login");
 			}
+		}
 
-			$this->load->view("admin/view_login");
+		public function logout() {
+			$this->session->sess_destroy();
+			redirect(site_url(array("c" => "admin", "m" => "index")));
 		}
 
 		public function main() {
-			$is_logged_in = $this->session->userdata("is_logged_in");
-			if ($is_logged_in == FALSE) {
-				redirect("c=admin&m=index", "refresh");
-			}
-
-			$this->load->model("tps_model");
-			$server = $this->input->get("server");
-			$type = $this->input->get("type");
-			$limit = $this->input->get("limit");
-
-			if (empty ($limit)) {
-				$limit = FALSE;
-			}
-
-			$this->load->library('gcharts');
-			$this->gcharts->load('LineChart');
-			$dataTable = $this->gcharts->DataTable('TPS');
-			$dataTable->addColumn('string', 'Timestamp', 'timestamp');
-			$dataTable->addColumn('number', 'TPS', 'tps');
-			$dataTable->addColumn('number', 'Player Count', 'playerCount');
-
-			$dataArray = $this->tps_model->get_tick_data($server, $type, $limit);
-
-			foreach ($dataArray as $data) {
-				$dataTable->addRow(array(
-					date("d/m/Y H:i:s", strtotime($data ["last_update"])), $data ["tps"], $data ["count"]
-				));
-			}
-
-			$this->gcharts->LineChart('TPS')->setConfig(array(
-				"title" => "TPS", 'hAxis' => new hAxis (array(
-						'textPosition' => 'out', 'slantedText' => TRUE, 'slantedTextAngle' => 45
-					))
-			));
-
-			$this->load->view("admin/view_graph");
-		}
-
-		// board/display/server/type
-		public function show() {
 			$this->load->model("tps_model");
 			$server = $this->input->get("server");
 			$type = $this->input->get("type");
@@ -95,7 +58,12 @@
 			$tps = $this->tps_model->get_tps($server, $type);
 			$players = implode(",", $this->tps_model->get_players($tps ["rowid"]));
 
-			$array = json_decode($data, TRUE);
+			if ($data == "") {
+				$array = array(array(), array(), array(), array(), array());
+			} else {
+				$array = json_decode($data, TRUE);
+			}
+
 			$entityArray = $array [1];
 			$chunkArray = $array [2];
 			$typeArray = $array [3];
@@ -136,10 +104,11 @@
 			$data = array(
 				"entities"    => $entityArray, "chunks" => $chunkArray, "types" => $typeArray, "calls" => $callArray,
 				"tps"         => $tps ["tps"], "last_update" => $tps ["last_update"], "players" => $players,
-				"playerCount" => count(explode(",", $players))
+				"playerCount" => count(explode(",", $players)), "script" => "admin/view_board_scripts",
+				"body"        => "admin/view_board"
 			);
 
-			$this->load->view("admin/view_board", $data);
+			$this->load->view("admin/view_template", $data);
 		}
 
 		public function graph() {
