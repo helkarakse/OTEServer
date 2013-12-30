bb1ea86f8ffa58d30620372ab670c19e24472e31
helkarakse <nimcuron@gmail.com>
2013-12-30 16:37:36 +0800

    Added main template for admin portal

---
+++ application/views/admin/view_template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Admin Portal</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<link rel="stylesheet" type="text/css" href="./css/horizontal-menu.css">
+	<link rel="stylesheet" type="text/css" href="./css/vertical-menu.css">
+	<script type="text/javascript"
+	        src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+</head>
+<body>
+<?php $this->load->view($body); ?>
+</body>
+</html>
