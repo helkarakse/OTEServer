cadb6f671fb7cac5f4716f81a0efee262e0915bf
helkarakse <nimcuron@gmail.com>
2013-12-31 16:11:54 +0800

    Updated base url in config for master branch

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://dev.otegamers.com/dev/';
+$config['base_url']	= 'http://dev.otegamers.com/helkarakse/';
 
 /*
 |--------------------------------------------------------------------------
