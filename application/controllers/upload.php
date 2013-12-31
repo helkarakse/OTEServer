1ee8c6d6c2a502d72ec9d862d8422bd0b0a2286e
helkarakse <nimcuron@gmail.com>
2013-12-31 22:51:29 +0800

    Updated upload controller to reflect the new db structure

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -9,12 +9,12 @@
 			$this->load->model("tps_model");
 			$server = $this->input->get("server");
 			$type = $this->input->get("type");
-			$timeNow = date("r", time());
+			$timeNow = time();
 
 			// decode and set the updated key
 			$array = json_decode(urldecode($this->input->post("json")));
 			$playerArray = json_decode(urldecode($this->input->post("players")));
-			$array [5] ["updated"] = $timeNow;
+			$array [5] ["updated"] = date("r", $timeNow);
 
 			// save the tps data and player list to db
 			foreach ($array [0] as $key => $value) {
