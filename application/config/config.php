b19685b15d092db010251ebf3c924e5868e5ebb2
helkarakse <nimcuron@gmail.com>
2013-12-31 22:42:08 +0800

    Updated base url for config again

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://dev.otegamers.com/helkarakse/';
+$config['base_url']	= 'http://dev.otegamers.com/';
 
 /*
 |--------------------------------------------------------------------------
