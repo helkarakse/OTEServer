70074b786d84a85bbf0049af50690f37e80bb472
helkarakse <nimcuron@gmail.com>
2014-01-01 18:19:15 +0800

    Updated links for the horizontal menu for logs

--- application/views/admin/view_log_directory.php
+++ application/views/admin/view_log_directory.php
@@ -2,15 +2,15 @@
 <div id="nav">
 	<ul id="menu">
 		<li><a href="<?php echo site_url(array(
-				"c" => "admin", "m" => "log_viewer", "server" => "rr", "type" => "1"
+				"c" => "admin", "m" => "log_viewer", "server" => "rr", "type" => "1", "log_type" => "crash"
 			)); ?>">Resonant
 				Rise 1</a></li>
 		<li><a href="<?php echo site_url(array(
-				"c" => "admin", "m" => "log_viewer", "server" => "rr", "type" => "2"
+				"c" => "admin", "m" => "log_viewer", "server" => "rr", "type" => "2", "log_type" => "crash"
 			)); ?>">Resonant
 				Rise 2</a></li>
 		<li><a href="<?php echo site_url(array(
-				"c" => "admin", "m" => "log_viewer", "server" => "ftb", "type" => "unleashed"
+				"c" => "admin", "m" => "log_viewer", "server" => "ftb", "type" => "unleashed", "log_type" => "crash"
 			)); ?>">FTB Unleashed</a></li>
 	</ul>
 </div>
