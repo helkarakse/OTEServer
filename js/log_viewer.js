4743ad110e827603e0c3938527ac33fd68709b4b
helkarakse <nimcuron@gmail.com>
2014-01-01 18:11:47 +0800

    Added new js file for log viewer

---
+++ js/log_viewer.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    setInterval("location.reload(true)", 1000 * 60 * 2);
+
+    $("div.tableCSS").hide();
+    $("div.tableCSS:first").show();
+    var newHeight = $("div.tableCSS:first").height() + 10 + $("div.mainText").height() + 10 + $("div.listCSS").height() + 10;
+    $("div#main").height(newHeight)
+
+    $("li.menuLink").click(function () {
+        var toShow = $(this).attr("display");
+        $("div.tableCSS").hide();
+        $("div#" + toShow).show();
+        $("div#main").height($("div#" + toShow).height() + 10 + $("div.mainText").height() + 10 + $("div.listCSS").height() + 10)
+    });
+});
