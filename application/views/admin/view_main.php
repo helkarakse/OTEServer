509f5b4b222895d5789094cc72eff2bf13ec34cd
helkarakse <nimcuron@gmail.com>
2014-01-02 23:43:00 +0800

    Initial commit for laravel codebase branch

--- application/views/admin/view_main.php
+++
@@ -1,9 +0,0 @@
-<div id="main">
-	<?php foreach ($data as $key => $value): ?>
-		<h2><?php echo $key; ?></h2>
-		<p>
-			TPS: <?php echo $value["tps"]; ?><br/>
-			Player Count: <?php echo $value["count"]; ?>
-		</p>
-	<?php endforeach; ?>
-</div>
